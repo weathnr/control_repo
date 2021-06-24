a7fd7768d95ee838c0c24dd3ec6807500de384e2
weathnr <86326929+weathnr@users.noreply.github.com>
2021-06-23 21:32:00 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,6 +2,7 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node {'web.nrw.vm':}
   dockeragent::node {'db.nrw.vm':}
+  dockeragent::node {'minetest.nrw.vm':}
   host {'web.nrw.vm':
     ensure  =>  present,
     ip      =>  '172.18.0.2',
